f8a1ff1b0fe026768fec423da74325bf5f826a5c
cStoneDev <carlosmiguelpiedraalvarez@gmail.com>
2024-11-26 22:53:09 -0500

    fix: path to LoginView in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AnimalsView from '@/models/animals/AnimalsView.vue';
-import LoginView from '@/models/animals/LoginView.vue';
+import LoginView from '@/models/LoginView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
