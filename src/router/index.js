e4a489fcecacb1be4b58f9564676b3602388f4df
DaryllLorenzo <vikingwarrior4444@gmail.com>
2024-11-27 22:02:23 -0500

    Modificado router index.js para agregar SidebarComp como persistent layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
 import AnimalsView from '@/models/animals/AnimalsView.vue';
 import LoginView from '@/models/LoginView.vue';
+import SidebarComp from '@/components/SidebarComp.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      /*path: "/",
       name: "home",
       component: HomeView,
     },
@@ -18,6 +20,23 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
+    },*/
+    
+    // Persistent Layout
+      path: '/',
+      component: SidebarComp, // Sidebar persistente
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: HomeView, // Renderizado en el slot
+        },
+        {
+          path: 'about',
+          name: 'About',
+          component: AboutView, // Renderizado en el slot
+        },
+      ],
     },
     {
       path: "/animals",
