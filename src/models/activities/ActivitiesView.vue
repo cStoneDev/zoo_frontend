db8abe1edb1785e0e4beb5052ea4ee40fd88548a
DaryllLorenzo <vikingwarrior4444@gmail.com>
2024-12-11 21:48:14 -0500

    feat: complete basic activities model implementation

--- src/models/activities/ActivitiesView.vue
+++ src/models/activities/ActivitiesView.vue
@@ -1,13 +1,122 @@
 <template>
-    <div>
-        Hola mundo        
-    </div>
+    <DataTable title="Lista de Actividades" 
+        :items="activitiesData" 
+        :headers="activityHeaders"
+        :buttons="activityButtons" 
+        :activity-default="activityDefault" @update="handleUpdate">
+        <template #dialog-content="{ item, mode }">
+            <ActivityFormEdit_View_Add :item="item" :mode="mode"
+                v-if="mode == 'edit' || mode == 'view' || mode == 'add'" />
+            <ActivityFormDelete :item="item" :mode="mode" v-if="mode == 'delete'" />
+        </template>
+    </DataTable>
 </template>
 
 <script setup>
-    import DataTable from "../../components/table/Table.vue";
+import {ref} from 'vue';
+import DataTable from "../../components/table/Table.vue";
+import ActivityFormEdit_View_Add from "./components/ActivityFormEdit_View_Add.vue";
+import ActivityFormDelete from "./components/ActivityFormDelete.vue";
+
+
+// Datos de animales
+const activitiesData = ref([
+    {
+        id: 1,
+        fecha: "2025-10-12",
+        hora: "06:05:00",
+        id_contrato: 5,
+        descripcion: "Muchacho pongase a trabajar"
+    },
+    {
+        id: 2,
+        fecha: "2025-11-12",
+        hora: "07:05:00",
+        id_contrato: 2,
+        descripcion: "Muchachote pongase a trabajar"
+    },
+    {
+        id: 3,
+        fecha: "2025-11-15",
+        hora: "10:05:00",
+        id_contrato: 1,
+        descripcion: "Muchacha pongase a trabajar"
+    },
+    {
+        id: 4,
+        fecha: "2025-12-12",
+        hora: "08:05:00",
+        id_contrato: 3,
+        descripcion: "Mano pongase a trabajar"
+    },
+    {
+        id: 5,
+        fecha: "2025-11-16",
+        hora: "07:23:00",
+        id_contrato: 9,
+        descripcion: "Pancito de la bodega"
+    }
+]);
+
+const activityDefault = ref({
+    id: 0,
+    fecha: "2024-0-0",
+    hora: "09:00:00",
+    id_contrato: 0,
+    descripcion: ""
+})
+// Encabezados de la tabla
+const activityHeaders = ref([
+    { title: "ID", value: "id", sortable: "true" },
+    { title: "Fecha", value: "fecha", sortable: "true" },
+    { title: "Contrato", value: "id_contrato", sortable: "true" }
+]);
+
+// Configuración de botones CRUD
+const activityButtons = ref([
+    {
+        text: "Ver",
+        icon: "mdi-eye",
+        mode: "view",
+
+    },
+    {
+        text: "Editar",
+        icon: "mdi-pencil",
+        mode: "edit",
+
+    },
+    {
+        text: "Agregar",
+        icon: "mdi-plus",
+        mode: "add",
+
+    },
+    {
+        text: "Eliminar",
+        icon: "mdi-delete",
+        mode: "delete",
+    },
+]);
+
+// Manejo de actualizaciones desde el hijo
+function handleUpdate({ mode, item }) {
+
+    if (mode === 'add') {
+        activitiesData.value.push({ id: Date.now(), ...item });
+    } else if (mode === 'edit') {
+        const index = activitiesData.value.findIndex((data) => data.id === item.id);
+        if (index !== -1) {
+            // Crear un nuevo arreglo con el elemento actualizado
+            activitiesData.value = activitiesData.value.map((data, i) =>
+                i === index ? { ...item } : data
+            );
+        }
+    } else if (mode === "delete") {
+        activitiesData.value = activitiesData.value.filter((data) => data.id !== item.id);
+    }
+}
+
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/models/activities/components/ActivityFormEdit_View_Add.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-form>
+      <v-text-field 
+        v-model="item.id" 
+        label="ID" 
+        type="number" 
+        required 
+        :readonly="mode === 'view'" 
+      />
+      <v-text-field 
+        v-model="item.id_contrato" 
+        label="Id Contrato" 
+        type="number" 
+        required 
+        :readonly="mode === 'view'" 
+      />
+      <v-text-field 
+        v-model="item.fecha" 
+        label="Fecha"
+        type="date" 
+        required 
+        :readonly="mode === 'view'" 
+      />
+      <v-text-field 
+        v-model="item.hora" 
+        label="Hora" 
+        type="time" 
+        required 
+        :readonly="mode === 'view'" 
+      />
+      <v-textarea 
+        v-model="item.descripcion" 
+        label="Descripcion" 
+        required 
+        :readonly="mode === 'view'" 
+      />
+    </v-form>
+  </template>
+  
+  <script setup>
+    import { defineProps } from "vue";
+  
+    defineProps({
+      item: {
+        type: Object,
+        default: () => ({}),
+      },
+      mode: {
+        type: String,
+        required: true,
+      },
+    });
+  </script>
+  
