38eb17ff797995d4c497249461ed33d48a2ee3b3
AndyClemente <andyclmentegago@gmail.com>
2024-12-13 23:24:48 -0500

    for pull

---
+++ src/models/users/UsersView.vue
@@ -0,0 +1,93 @@
+<template>
+    <DataTable title="Lista de Usuarios" :items="usersData" :headers="usersHeaders" :buttons="usersButtons"
+        :users-default="usersDefault" @update="handleUpdate">
+        <template #dialog-content="{ item, mode }">
+            <UsersFormEdit_View_Add :item="item" :mode="mode" v-if="mode == 'edit' || mode == 'view' || mode == 'add'" />
+            <UsersFormDelete :item="item" :mode="mode" v-if="mode == 'delete'" />
+        </template>
+    </DataTable>
+</template>
+
+<script setup>
+import DataTable from "../../components/table/Table.vue";
+import UsersFormEdit_View_Add from "./components/UsersFormEdit_View_Add.vue";
+import UsersFormDelete from "./components/UsersFormDelete.vue";
+import { ref } from "vue";
+
+// Datos de animales
+const usersData = ref([
+    {
+        id: 1,
+        rol: "Moderador",
+        nombre: "Jose",
+    },
+    {
+        id: 2,
+        rol: "Moderador",
+        nombre: "Juan",
+    },
+    {
+        id: 3,
+        rol: "Admin",
+        nombre: "Pepe",
+    },
+]);
+
+const userDefault = ref({
+    id: 0,
+    rol: "",
+    nombre: "",
+})
+// Encabezados de la tabla
+const animalHeaders = ref([
+    { title: "ID", value: "id", sortable: "true" },
+    { title: "Nombre", value: "nombre", sortable: "true" },
+    { title: "Rol", value: "rol", sortable: "true" },
+]);
+
+// Configuración de botones CRUD
+const usersButtons = ref([
+    {
+        text: "Ver",
+        icon: "mdi-eye",
+        mode: "view",
+
+    },
+    {
+        text: "Editar",
+        icon: "mdi-pencil",
+        mode: "edit",
+
+    },
+    {
+        text: "Agregar",
+        icon: "mdi-plus",
+        mode: "add",
+
+    },
+    {
+        text: "Eliminar",
+        icon: "mdi-delete",
+        mode: "delete",
+    },
+]);
+
+// Manejo de actualizaciones desde el hijo
+function handleUpdate({ mode, item }) {
+
+    if (mode === 'add') {
+        usersData.value.push({ id: Date.now(), ...item });
+    } else if (mode === 'edit') {
+        const index = usersData.value.findIndex((data) => data.id === item.id);
+        if (index !== -1) {
+            // Crear un nuevo arreglo con el elemento actualizado
+            usersData.value = usersData.value.map((data, i) =>
+                i === index ? { ...item } : data
+            );
+        }
+    } else if (mode === "delete") {
+        usersData.value = usersData.value.filter((data) => data.id !== item.id);
+    }
+}
+
+</script>
---
+++ src/models/users/components/UsersFormEdit_View_Add.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-form>
+      <v-text-field
+        v-model="item.user"
+        label="Usuario"
+        required
+        :readonly="mode === 'view'"
+      />
+      <v-text-field
+        v-model="item.rol"
+        label="Rol"
+        required
+        :readonly="mode === 'view'"
+      />
+
+
+    </v-form>
+  </template>
+  
+  <script setup>
+    import { defineProps } from "vue";
+  
+    defineProps({
+      item: {
+        type: Object,
+        default: () => ({}),
+      },
+      mode: {
+        type: String,
+        required: true,
+      },
+    });
+  </script>
+  
