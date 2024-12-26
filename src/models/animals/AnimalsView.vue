cfe8884c24e8c5d618f843505ce1589fd4582a75
AndyMAG <andymartinezag19@gmail.com>
2024-12-26 18:55:04 -0500

    refactor(table): Firsts changes of AnimalsView

--- src/models/animals/AnimalsView.vue
+++ src/models/animals/AnimalsView.vue
@@ -1,6 +1,13 @@
 <template>
-  <DataTable title="Lista de Animales" :items="animalData" :headers="animalHeaders" :buttons="animalButtons"
-    :animal-default="animalDefault" :filters="animalFilters" @update="handleUpdate">
+  <DataTable 
+    title="Lista de Animales" 
+    :headers="animalHeaders" 
+    :items="animalData" 
+    :itemsLength="currentPageData"
+    :animal-default="animalDefault" 
+    :filters="animalFilters"
+    @update="getAnimalsFromService"
+> 
     <template #dialog-content="{ item, mode }">
       <AnimalFormEdit_View_Add :item="item" :mode="mode" v-if="mode == 'edit' || mode == 'view' || mode == 'add'" />
       <AnimalFormDelete :item="item" :mode="mode" v-if="mode == 'delete'" />
@@ -16,103 +23,94 @@ import AnimalFormDelete from "./components/AnimalFormDelete.vue";
 import { ref, reactive, onMounted } from "vue";
 import animalService from "../animals/animalService"; // Asegúrate de importar el servicio correctamente
 
+import { usePageStore } from '/src/stores/page.js'
 // Datos de animales
 const animalData = ref([]);
+
+const currentPageData = ref(0);
+const totalElementsData  = ref(0);
+const totalPagesData  = ref(0);
+const pageSizeData  = ref(0);
+
+const itemsPerPage = ref(10);
+
+const pageStore = usePageStore();
+
 
 const animalFilters = reactive({
   breedId: {
     lista: [],
-    label: "ID raza"
+    label: "Raza"
   },
   speciesId: {
     lista: [],
-    label: "ID especie"
+    label: "Especie"
   },
   minAge: {
-    lista: [],
+
     label: "Edad Mínima"
   },
   maxAge: {
-    lista: [],
+
     label: "Edad Máxima"
   },
   minWeight: {
-    lista: [],
+
     label: "Peso Mínimo"
   },
   maxWeight: {
-    lista: [],
+
     label: "Peso Máximo"
   },
   minDaysInShelter: {
-    lista: [],
+
     label: "Días en refugio mínimo"
   },
   maxDaysInShelter: {
-    lista: [],
+
     label: "Días en refugio máximo"
   },
 });
 
 const animalDefault = ref({
-  id: 0,
-  nombre: "",
-  id_raza: 0,
-  id_especie: 0,
-  edad: 0,
-  peso: 0,
-  dias_refugio: 0,
+    id: 0,
+    name: "",
+    age: 0,
+    breedId: 0,
+    breedName: "",
+    entryDate:"",
+    specieId:0,
+    specieName:"",
+    weight:0,
 });
 
 // Encabezados de la tabla
 const animalHeaders = ref([
-  { title: "Raza", value: "id_raza", sortable: "true" },
-  { title: "Especie", value: "id_especie", sortable: "true" },
-  { title: "Nombre", value: "nombre", sortable: "true" },
-  { title: "Edad (años)", value: "edad", sortable: "true" },
-  { title: "Peso (kg)", value: "peso", sortable: "true" },
-  { title: "Días en Zoológico", value: "dias_refugio", sortable: "true" },
+  { title: "Nombre", value: "name", sortable: "false" },
+  { title: "Edad (años)", value: "age", sortable: "false" },
+  { title: "Raza", value: "breedName", sortable: "false" },
+  { title: "Especie", value: "specieName", sortable: "false" },
+  { title: "Peso (kg)", value: "weight", sortable: "false" },
   { title: "Acciones", value: "actions", align: "center" },
 ]);
 
-// Configuración de botones CRUD
-const animalButtons = ref([
-  { text: "Agregar", icon: "mdi-plus", mode: "add" },
-]);
 
-// Manejo de actualizaciones desde el hijo
-function handleUpdate({ mode, item }) {
-  if (mode === 'add') {
-    animalData.value.push({ id: Date.now(), ...item });
-  } else if (mode === 'edit') {
-    const index = animalData.value.findIndex((data) => data.id === item.id);
-    if (index !== -1) {
-      animalData.value = animalData.value.map((data, i) =>
-        i === index ? { ...item } : data
-      );
-    }
-  } else if (mode === "delete") {
-    animalData.value = animalData.value.filter((data) => data.id !== item.id);
-  }
-}
 
 // Obtener animales desde el servicio
 const getAnimalsFromService = async () => {
   try {
-    const { animals } = await animalService.getAnimals(0, 20); // Ajustar los parámetros de la paginación si es necesario
+    const { animals, pagination } = await animalService.getAnimals(0, pageStore.itemsPerPage); // Ajusta los parámetros de la paginación si es necesario
+    const { totalElements, totalPages, currentPage, pageSize } = pagination;
+    
+    console.log("yo pido " + pageStore.itemsPerPage)
     animalData.value = animals;
 
-    console.log(animalData);
-
+    currentPageData.value = totalElements;
+    totalElementsData.value  = totalPages;
+    totalPagesData.value  = currentPage;
+    pageSizeData.value  = pageSize;
     // Actualizar filtros basados en los datos obtenidos
-    animalFilters.breedId.lista = animals.map(item => item.id_raza).sort((a, b) => a - b);
-    animalFilters.speciesId.lista = animals.map(item => item.id_especie).sort((a, b) => a - b);
-    animalFilters.minAge.lista = animals.map(item => item.edad).sort((a, b) => a - b);
-    animalFilters.maxAge.lista = animals.map(item => item.edad).sort((a, b) => a - b);
-    animalFilters.minWeight.lista = animals.map(item => item.peso).sort((a, b) => a - b);
-    animalFilters.maxWeight.lista = animals.map(item => item.peso).sort((a, b) => a - b);
-    animalFilters.minDaysInShelter.lista = animals.map(item => item.dias_refugio).sort((a, b) => a - b);
-    animalFilters.maxDaysInShelter.lista = animals.map(item => item.dias_refugio).sort((a, b) => a - b);
+
   } catch (error) {
     console.error('Error al cargar los animales:', error);
   }
